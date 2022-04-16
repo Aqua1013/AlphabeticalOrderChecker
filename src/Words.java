5dd9b121658e07ed0408889e55d9cf32ebfe6971
Aqua1013 <57598565+Aqua1013@users.noreply.github.com>
2022-04-15 23:58:01 -0500

    Add files via upload

---
+++ src/Words.java
@@ -0,0 +1,27 @@
+import com.sun.istack.internal.NotNull;
+
+import java.util.Arrays;
+
+public final class Words {
+
+    /**
+     * @param word
+     * Returns true of string is in alphabetical order, if not returns false
+     * Takes the input of a word or sentence and checks if it is in alphabetical order from start to finish.
+     */
+
+    @Deprecated
+    @NotNull
+    public static boolean isAlphabeticalOrder(char[] word){
+        for (char c : word) if (c < word[0] && c != ' ') return false;
+        return true;
+    }
+
+    @NotNull
+    public static boolean isAlphabeticalOrder(String word){
+        char[] temp = word.toLowerCase().toCharArray();
+        for (char c : temp) if (c < temp[0] && c != ' ') return false;
+        return true;
+    }
+
+}
