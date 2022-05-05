fdf880f526e239b093db7ae0096e351417cb01cc
Aqua1013 <57598565+Aqua1013@users.noreply.github.com>
2022-05-05 09:05:36 -0500

    Update StringUtil.java

--- src/StringUtil.java
+++ src/StringUtil.java
@@ -7,38 +7,36 @@ public final class StringUtil extends String{
      * Returns true of string is in alphabetical order, if not returns false
      * Takes the input of a word or sentence and checks if it is in alphabetical order from start to finish.
      */
+    
+    private String string;
 
     public StringUtil(String string){
-        super(string);
-    }    
-    
-    public void setString(String string){
-        this = string;
+        this.string = string;
     }
     
     public String getString(){
-       return this;
+       return string;
     }
     
     @Deprecated
     @NotNull
     public boolean isAlphabeticalOrder(){
-        String word = this;
+        char[] word = string.toCharArray();
         for (char c : word) if (c < word[0] && c != ' ') return false;
         return true;
     }
     
     @Deprecated
     public boolean isAlphabeticalOrder(){
-        char[] temp = this.toLowerCase().toCharArray();
+        char[] temp = string.toLowerCase().toCharArray();
         for(char c : temp)
             if (c < temp[0] && c != ' ') return false;
         return true;
     }
     
     public boolean isAlphabeticalOrder(){
-        if(this.equals(null)) return false;
-        String word = this.toLowerCase();
+        if(string.equals(null)) return false;
+        String word = string.toLowerCase();
         char bound = word.charAt(0);
         for(int i = 0; i < word.length(); i++){
             char c = word.charAt(i);
